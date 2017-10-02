6f071a6c2f5d067c42ae8aadf462b7b6fc5bd45b
Severiano Badajoz <sevebadajoz@gmail.com>
2017-10-01 17:29:07 -0700

    Initial commit

---
+++ Project1/Functions.cpp
@@ -0,0 +1,22 @@
+/*
+	Name header
+*/
+
+#include "DoublyList.h"
+
+// Definition function print
+
+
+// Definition function reversePrint
+
+
+// Definition function front
+
+
+// Definition function back
+
+
+// Definition function copyToList
+
+
+// Definition function insertInOrder 
---
+++ Project1/Main.cpp
@@ -0,0 +1,62 @@
+/***********************************************
+
+	Class DoublyList
+
+************************************************/
+
+#include "DoublyList.h"
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	DoublyList intList;
+
+	intList.insertInOrder(37);
+	intList.insertInOrder(53);
+	intList.insertInOrder(21);
+	intList.insertInOrder(18);
+	intList.insertInOrder(73);
+	intList.insertInOrder(49);
+	
+	cout << "TESTING: insertInOrder() and print()" 
+		<< "\n\nEXPECTED OUTPUT: "
+		<< "\nList: 18  21  37  49  53  73\n";
+	cout << "\nACTUAL OUTPUT:";
+	cout << "\nList: ";
+	intList.print();
+
+	cout << "\n\n-----------------------------------------------------\n";
+	cout << "TESTING: printReverse()" 
+		<< "\n\nEXPECTED OUTPUT: "
+		<< "\nList (reversed): 73  53  49  37  21  18\n";
+	cout << "\nACTUAL OUTPUT:";
+	cout << "\nList (reversed): ";
+	intList.reversePrint();
+
+	cout << "\n\n-----------------------------------------------------\n";
+	cout << "FINAL TESTING: Check first and last nodes" 
+		<< "\n\nEXPECTED OUTPUT: "
+		<< "\nFirst node data: 18"
+		<< "\nLast node data: 73\n";
+	cout << "\nACTUAL OUTPUT:";
+	cout << "\nFirst node data: "
+		<< intList.front()
+		<< "\nLast node data: "
+		<< intList.back() << endl;
+
+	intList.destroyList();	
+	
+	cout << "\nYour testing cases...\n";
+	// TEST function copyToList
+	// Create two lists, list1 and list2.
+	// Copy from empty list list1.
+	// Add elements to list1.
+	// Copy from list1 to empty list list2.
+	// Copy from list1 to list2.
+
+	cout << endl << endl;
+	system("Pause");
+	return 0;
+}
